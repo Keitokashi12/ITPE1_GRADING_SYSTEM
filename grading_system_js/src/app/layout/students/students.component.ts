c0a6746d111ee5263da9146448a8366da17f4311
Keitokashi12 <144686355+Keitokashi12@users.noreply.github.com>
2023-10-20 21:43:45 +0800

    Added a Teacherrs and Students

--- grading_system_js/src/app/layout/students/students.component.ts
+++ grading_system_js/src/app/layout/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { card_data } from 'src/shared_data/student_data';
 
 @Component({
   selector: 'app-students',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
-
+  cardData = card_data;
   constructor() { }
+
 
   ngOnInit(): void {
   }
